04ca914b021c4e05fc5b29f2f8b050f0dd0ace48
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-06-01 11:32:06 -0700

    Add files via upload

---
+++ entitytype.cs
@@ -0,0 +1,9 @@
+
+using UnityEngine;
+
+public class entitytype : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public enum EntityTypes {logo, nuc}
+    public EntityTypes entityType;
+}
